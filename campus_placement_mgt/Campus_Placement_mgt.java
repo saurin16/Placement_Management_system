141867d30fcf43301682870bb3a9014d5f248168
saurin16 <61411800+saurin16@users.noreply.github.com>
2021-02-28 11:26:57 +0530

    Add files via upload

---
+++ campus_placement_mgt/Campus_Placement_mgt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campus_placement_mgt;
+
+
+public class Campus_Placement_mgt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainformYorN m=new MainformYorN();
+        m.setVisible(true);
+    }
+    
+}
